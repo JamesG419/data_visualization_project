c20a7b26dc97eb4c245adda3dec3d132d0377c82
James Glenister <jglen419@gmail.com>
2019-01-29 20:03:09 -0500

    added looping logic and price scraping

--- scrapper.py
+++ scrapper.py
@@ -12,3 +12,18 @@ car_models = ['escape', 'cr-v','santa-fe', 'rav-4']
 year = '2017'
 
 
+for i in car_makes:
+    weblink = edmunds + car_makes[i] + '/' + car_models[i] + '/' + year
+
+    page = requests.get(weblink, headers = {'User-Agent' : 'Mozilla/5.0'})
+    html = page.text 
+    soup = BeautifulSoup(html, 'html.parser')
+
+    #pulls price data
+    price_soup = soup.find_all('div', class_ = 'size-30 font-weight-bold text-info text-left')
+    price_string = price_soup[0].text
+    price_string_split = price_string.split(' - ')
+    resale_price = price_string_split[0]
+    msrp = price_string_split[1]
+
+    
